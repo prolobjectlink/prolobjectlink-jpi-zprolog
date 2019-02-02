a5b12b2ac43e3f425e5ca0439f179e40493388c3
Jose Zalacain <Jose Zalacain@10.11.0.25>
2019-02-02 11:41:58 -0400

    Replace WorkLogic by Prolobjectlink

---
+++ src/main/java/org/prolobjectlink/prolog/zprolog/ZPrologConverter.java
@@ -0,0 +1,128 @@
+/*
+ * #%L
+ * prolobjectlink-jpi-zprolog
+ * %%
+ * Copyright (C) 2019 Prolobjectlink Project
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package org.prolobjectlink.prolog.zprolog;
+
+import static org.prolobjectlink.prolog.PrologTermType.ATOM_TYPE;
+import static org.prolobjectlink.prolog.PrologTermType.CUT_TYPE;
+import static org.prolobjectlink.prolog.PrologTermType.DOUBLE_TYPE;
+import static org.prolobjectlink.prolog.PrologTermType.EMPTY_TYPE;
+import static org.prolobjectlink.prolog.PrologTermType.FAIL_TYPE;
+import static org.prolobjectlink.prolog.PrologTermType.FALSE_TYPE;
+import static org.prolobjectlink.prolog.PrologTermType.FLOAT_TYPE;
+import static org.prolobjectlink.prolog.PrologTermType.INTEGER_TYPE;
+import static org.prolobjectlink.prolog.PrologTermType.LIST_TYPE;
+import static org.prolobjectlink.prolog.PrologTermType.LONG_TYPE;
+import static org.prolobjectlink.prolog.PrologTermType.NIL_TYPE;
+import static org.prolobjectlink.prolog.PrologTermType.STRUCTURE_TYPE;
+import static org.prolobjectlink.prolog.PrologTermType.TRUE_TYPE;
+import static org.prolobjectlink.prolog.PrologTermType.VARIABLE_TYPE;
+import static org.prolobjectlink.prolog.zprolog.ZPrologTerm.CUT_TERM;
+import static org.prolobjectlink.prolog.zprolog.ZPrologTerm.EMPTY_TERM;
+import static org.prolobjectlink.prolog.zprolog.ZPrologTerm.FAIL_TERM;
+import static org.prolobjectlink.prolog.zprolog.ZPrologTerm.FALSE_TERM;
+import static org.prolobjectlink.prolog.zprolog.ZPrologTerm.NIL_TERM;
+import static org.prolobjectlink.prolog.zprolog.ZPrologTerm.TRUE_TERM;
+
+import org.prolobjectlink.prolog.AbstractConverter;
+import org.prolobjectlink.prolog.PrologAtom;
+import org.prolobjectlink.prolog.PrologConverter;
+import org.prolobjectlink.prolog.PrologDouble;
+import org.prolobjectlink.prolog.PrologFloat;
+import org.prolobjectlink.prolog.PrologInteger;
+import org.prolobjectlink.prolog.PrologLong;
+import org.prolobjectlink.prolog.PrologProvider;
+import org.prolobjectlink.prolog.PrologTerm;
+import org.prolobjectlink.prolog.PrologVariable;
+import org.prolobjectlink.prolog.UnknownTermError;
+
+public class ZPrologConverter extends AbstractConverter<ZPrologTerm> implements PrologConverter<ZPrologTerm> {
+
+	public PrologTerm toTerm(ZPrologTerm term) {
+		return term;
+	}
+
+	public ZPrologTerm fromTerm(PrologTerm term) {
+		switch (term.getType()) {
+		case NIL_TYPE:
+			return NIL_TERM;
+		case CUT_TYPE:
+			return CUT_TERM;
+		case FAIL_TYPE:
+			return FAIL_TERM;
+		case TRUE_TYPE:
+			return TRUE_TERM;
+		case FALSE_TYPE:
+			return FALSE_TERM;
+		case EMPTY_TYPE:
+			return EMPTY_TERM;
+		case ATOM_TYPE:
+			return new ZPrologTerm(provider, ((PrologAtom) term).getStringValue());
+		case FLOAT_TYPE:
+			return new ZPrologTerm(provider, ((PrologFloat) term).getFloatValue());
+		case INTEGER_TYPE:
+			return new ZPrologTerm(provider, ((PrologInteger) term).getIntValue());
+		case DOUBLE_TYPE:
+			return new ZPrologTerm(provider, ((PrologDouble) term).getDoubleValue());
+		case LONG_TYPE:
+			return new ZPrologTerm(provider, ((PrologLong) term).getLongValue());
+		case VARIABLE_TYPE:
+			PrologVariable v = (PrologVariable) term;
+			String name = v.getName();
+			ZPrologTerm variable = sharedPrologVariables.get(name);
+			if (variable == null) {
+				variable = new ZPrologTerm(provider, name, v.getPosition());
+				sharedPrologVariables.put(name, variable);
+			}
+			return variable;
+		case LIST_TYPE:
+			return new ZPrologTerm(provider, term.getArguments());
+		case STRUCTURE_TYPE:
+			String functor = term.getFunctor();
+			return new ZPrologTerm(provider, functor, term.getArguments());
+		default:
+			throw new UnknownTermError(term);
+		}
+	}
+
+	public ZPrologTerm[] fromTermArray(PrologTerm[] terms) {
+		ZPrologTerm[] prologTerms = new ZPrologTerm[terms.length];
+		for (int i = 0; i < terms.length; i++) {
+			prologTerms[i] = fromTerm(terms[i]);
+		}
+		return prologTerms;
+	}
+
+	public ZPrologTerm fromTerm(PrologTerm head, PrologTerm[] body) {
+		ZPrologTerm clauseHead = fromTerm(head);
+		if (body != null && body.length > 0) {
+			ZPrologTerm clauseBody = fromTerm(body[body.length - 1]);
+			for (int i = body.length - 2; i >= 0; --i) {
+				clauseBody = new ZPrologTerm(provider, ",", fromTerm(body[i]), clauseBody);
+			}
+			return new ZPrologTerm(provider, ":-", clauseHead, clauseBody);
+		}
+		return clauseHead;
+	}
+
+	public PrologProvider createProvider() {
+		return new ZProlog(this);
+	}
+
+}
---
+++ src/main/java/org/prolobjectlink/prolog/zprolog/ZPrologMachine.java
@@ -0,0 +1,104 @@
+/*
+ * #%L
+ * prolobjectlink-jpi-zprolog
+ * %%
+ * Copyright (C) 2019 Prolobjectlink Project
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package org.prolobjectlink.prolog.zprolog;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.prolobjectlink.prolog.AbstractEngine;
+import org.prolobjectlink.prolog.PrologProvider;
+import org.prolobjectlink.prolog.PrologTerm;
+
+abstract class ZPrologMachine extends AbstractEngine {
+
+	// prolog flags container
+	private final Map<String, ZPrologFlag> flags;
+
+	protected ZPrologMachine(PrologProvider provider) {
+		super(provider);
+
+		flags = new HashMap<String, ZPrologFlag>();
+
+		// defaults flag
+		flags.put("debug", ZPrologFlag.DEBUG_FLAG);
+		flags.put("unknown", ZPrologFlag.UNKNOWN_FLAG);
+		flags.put("bounded", ZPrologFlag.BOUNDED_FLAG);
+		flags.put("max_arity", ZPrologFlag.MAX_ARITY_FLAG);
+		flags.put("max_integer", ZPrologFlag.MAX_INTEGER_FLAG);
+		flags.put("min_integer", ZPrologFlag.MIN_INTEGER_FLAG);
+		flags.put("double_quotes", ZPrologFlag.DOUBLE_QUOTES_FLAG);
+		flags.put("char_conversion", ZPrologFlag.CHAR_CONVERSION_FLAG);
+		flags.put("integer_rounding_function", ZPrologFlag.INTEGER_ROUNDING_FUNCTION_FLAG);
+
+	}
+
+	protected final Map<String, ZPrologFlag> getFlags() {
+		return flags;
+	}
+
+	protected final boolean setFlag(String name, PrologTerm value) {
+		ZPrologFlag flag = flags.get(name);
+		if (flag.isChangeable()) {
+			flag.setValue(value);
+			flags.put(name, flag);
+			return true;
+		}
+		return false;
+	}
+
+	protected final PrologTerm currentFlag(String name, PrologTerm value) {
+		return flags.get(name).getValue();
+	}
+
+	protected final void defineFlag(String name, PrologTerm value, PrologTerm defaultValue, boolean changeable) {
+		flags.put(name, new ZPrologFlag(name, value, defaultValue, changeable));
+	}
+
+	protected final boolean isChangeable(String name) {
+		return flags.get(name).isChangeable();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((flags == null) ? 0 : flags.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ZPrologMachine other = (ZPrologMachine) obj;
+		if (flags == null) {
+			if (other.flags != null)
+				return false;
+		} else if (!flags.equals(other.flags)) {
+			return false;
+		}
+		return true;
+	}
+
+}
